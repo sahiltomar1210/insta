5b5b71deee93d51fefbe8a60632cfeadca303492
Sahil Tomar <113227437+sahiltomar1210@users.noreply.github.com>
2022-12-21 12:39:48 +0530

    Update Post-view.js

--- src/Component/Post-view/Post-view.js
+++ src/Component/Post-view/Post-view.js
@@ -9,8 +9,15 @@ const PostView = () => {
     const[posts, setPosts] = useState([]);
 
     useEffect(()=>{
-        fetch("http://localhost:3004/data").then((res)=>res.json()).then((data)=>{
-            setPosts(data);
+      fetch('https://api.jsonbin.io/v3/b/63a2a6a9dfc68e59d56d8eb4/latest', {
+        method: 'GET',
+        headers: {
+            'X-Master-Key':'$2b$10$PxAyfYRwvxBYPaHAfXV1puqzsO3U9Mcc0Zl3u44CqzpOdyS07RGSG',
+            'X-Access-Key':'$2b$10$yWqhrNTN5PSX9slzND0Ad.uV0IMQ3JV39Z8vlHE/N3P9uMXI8zRdG',
+            'X-Bin-Meta':'false'
+          }
+        }).then((res)=>res.json()).then((data)=>{
+            setPosts(data.data);
         }).catch((err)=>{
             if(err){
                 console.log(err);            }
